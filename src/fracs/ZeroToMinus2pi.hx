372d6c62007fbd286be2fe9b0494bbd7d523855b
nanjizal <nanjizal@users.noreply.github.com>
2019-12-24 09:06:17 +0000

    Update ZeroToMinus2pi.hx

--- src/fracs/ZeroToMinus2pi.hx
+++ src/fracs/ZeroToMinus2pi.hx
@@ -1,5 +1,5 @@
-package fracs.angle;
-import fracs.angle.Fraction;
+package fracs;
+import fracs.Fraction;
 @forward
 abstract ZeroToMinus2pi( Float ) to Float {
     inline // private can't create directly
