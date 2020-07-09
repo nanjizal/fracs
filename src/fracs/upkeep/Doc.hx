81b05356f7445ffde5bcbcf6dccc3bf4814d220f
nanjizal <none>
2020-07-09 16:31:00 +0100

    add dox

---
+++ src/fracs/upkeep/Doc.hx
@@ -0,0 +1,16 @@
+package fracs.upkeep;
+import fracs.Angles;
+import fracs.Fraction;
+import fracs.Pi2pi;
+import fracs.ZeroTo2pi;
+import fracs.ZeroToMinus2pi;
+
+@:dox(hide)
+class Doc {
+    public static function main(){
+        
+    }
+    public function new(){
+        
+    }
+}
